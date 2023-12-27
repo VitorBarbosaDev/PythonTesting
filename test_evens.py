da34ae822a0f9eeee0ca73315c6f570d318d51e7
VitorBarbosa1 <VITORBARBOSA1301@GMAIL.COM>
2023-12-27 13:41:18 +0000

    Removed Unnessary lines

--- test_evens.py
+++ test_evens.py
@@ -3,22 +3,14 @@ from evens import even_number_of_evens
 
 
 class TestEvens(unittest.TestCase):
+    def test_throws_error_if_value_passed_in_is_not_list(self):
+        self.assertRaises(TypeError, even_number_of_evens, 4)
 
-    def test_even_number_of_evens(self):
-        self.assertEqual(even_number_of_evens(5), False)
-        self.assertEqual(even_number_of_evens(10), True)
-        self.assertEqual(even_number_of_evens(15), False)
-        self.assertEqual(even_number_of_evens(20), True)
-        self.assertEqual(even_number_of_evens(25), False)
-        self.assertEqual(even_number_of_evens(30), True)
-        self.assertEqual(even_number_of_evens(35), False)
-        self.assertEqual(even_number_of_evens(40), True)
-        self.assertEqual(even_number_of_evens(45), False)
-        self.assertEqual(even_number_of_evens(50), True)
-        self.assertEqual(even_number_of_evens(55), False)
-        self.assertEqual(even_number_of_evens(60), True)
-        self.assertEqual(even_number_of_evens(65), False)
-
+    def test_values_in_list(self):
+        self.assertEqual(even_number_of_evens([]), False)
+        self.assertEqual(even_number_of_evens([2, 4]), True)
+        self.assertEqual(even_number_of_evens([2]), False)
+        self.assertEqual(even_number_of_evens([1, 3, 5]), False)
 
 
 if __name__ == "__main__":
